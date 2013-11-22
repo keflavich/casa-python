c731a54224a53a0d73cd2f47636ee1639fb989e2
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2013-11-22 12:13:57 +0100

    added pip installation script & casa-pip tool

---
+++ install_pip.sh
@@ -0,0 +1,4 @@
+curl -O https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py
+curl -O https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+./casa-python ez_setup.py
+./casa-python get-pip.py
